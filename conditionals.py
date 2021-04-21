f188baf145025ec09bd5fa3fa95e65f860c89e62
Javier caballero <javiereduardocaballero@gmail.com>
2021-04-21 08:54:11 -0300

    First version of conditionals

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+# Basic example of conditionals with python by Javicab2
+
+
+#TODO: Enter a number between 0 and 10
+
+print ("Please enter a number")
+
+number = int(input())
+
+if number > 10 :
+  print("the number is greater than 10")
+
+else:
+  print("the number is lower than 10")
